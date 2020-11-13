db6c28d2ea6a04affde8ccf732ae0003a763d459
guanguans <ityaozm@gmail.com>
2020-11-13 17:26:29 +0800

    Add load command able

---
+++ src/Coole/Able/LoadCommandAble.php
@@ -0,0 +1,24 @@
+<?php
+
+/*
+ * This file is part of the guanguans/coole.
+ *
+ * (c) guanguans <ityaozm@gmail.com>
+ *
+ * This source file is subject to the MIT license that is bundled.
+ */
+
+namespace Guanguans\Coole\Able;
+
+use Guanguans\Coole\App;
+use Guanguans\Coole\Console\CommandDiscoverer;
+
+trait LoadCommandAble
+{
+    public function loadCommand(string $dir, string $namespace, string $suffix = '*Command.php')
+    {
+        $commandDiscoverer = new CommandDiscoverer($dir, $namespace, $suffix);
+
+        return App::getInstance()['command']->add($commandDiscoverer->getCommands());
+    }
+}
