6ed73b28937ebde60e662deea9993ac8436b0908
guanguans <ityaozm@gmail.com>
2020-11-17 16:11:28 +0800

    Add Facade for Logger

---
+++ src/Coole/Facade/Logger.php
@@ -0,0 +1,19 @@
+<?php
+
+/*
+ * This file is part of the guanguans/coole.
+ *
+ * (c) guanguans <ityaozm@gmail.com>
+ *
+ * This source file is subject to the MIT license that is bundled.
+ */
+
+namespace Guanguans\Coole\Facade;
+
+class Logger extends Facade
+{
+    public static function getFacadeAccessor()
+    {
+        return 'logger';
+    }
+}
