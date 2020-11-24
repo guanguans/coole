f791c03b1605ae96a34a56061c2bbeae4a7b140c
guanguans <ityaozm@gmail.com>
2020-11-24 11:45:57 +0800

    Add annotate

--- src/Coole/Facade/Router.php
+++ src/Coole/Facade/Router.php
@@ -12,6 +12,31 @@ declare(strict_types=1);
 
 namespace Guanguans\Coole\Facade;
 
+/**
+ * @method static \Guanguans\Coole\Routing\Router any(string $pattern, array|string|callable|null $to = null)
+ * @method static \Guanguans\Coole\Routing\Router delete(string $pattern, array|string|callable|null $to = null)
+ * @method static \Guanguans\Coole\Routing\Router get(string $pattern, array|string|callable|null $to = null)
+ * @method static \Guanguans\Coole\Routing\Router match(string $pattern, array|string|callable|null $to = null)
+ * @method static \Guanguans\Coole\Routing\Router options(string $pattern, array|string|callable|null $to = null)
+ * @method static \Guanguans\Coole\Routing\Router patch(string $pattern, array|string|callable|null $to = null)
+ * @method static \Guanguans\Coole\Routing\Router post(string $pattern, array|string|callable|null $to = null)
+ * @method static \Guanguans\Coole\Routing\Router put(string $pattern, array|string|callable|null $to = null)
+ * @method static \Guanguans\Coole\Routing\Route setPath(string $pattern)
+ * @method static \Guanguans\Coole\Routing\Route setHost(?string $pattern)
+ * @method static \Guanguans\Coole\Routing\Route setSchemes(string|string[] $schemes)
+ * @method static \Guanguans\Coole\Routing\Route setMethods(string|string[] $methods)
+ * @method static \Guanguans\Coole\Routing\Route setOptions(array $options)
+ * @method static \Guanguans\Coole\Routing\Route setOption(string $name, $value)
+ * @method static \Guanguans\Coole\Routing\Route setDefaults(array $defaults)
+ * @method static \Guanguans\Coole\Routing\Route setDefault(string $name, $default)
+ * @method static \Guanguans\Coole\Routing\Route setRequirements(array $requirements)
+ * @method static \Guanguans\Coole\Routing\Route setRequirement(string $key, string $regex)
+ * @method static \Guanguans\Coole\Routing\Route setCondition(?string $condition)
+ * @method static \Guanguans\Coole\Routing\RouteRegistrar prefix(string  $prefix)
+ * @method static \Guanguans\Coole\Routing\RouteRegistrar group(array $attributes, callable $routes)
+ *
+ * @see \Guanguans\Coole\Routing\Router
+ */
 class Router extends Facade
 {
     public static function getFacadeAccessor()
