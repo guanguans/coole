20f757322399cfb2b0bab8d12f24a36271ca0122
yaozm <ityaozm@gmail.com>
2021-06-21 18:17:06 +0800

    Add IDE helper files

---
+++ _ide_helper.php
@@ -0,0 +1,32 @@
+<?php
+
+/*
+ * This file is part of the guanguans/coole.
+ *
+ * (c) guanguans <ityaozm@gmail.com>
+ *
+ * This source file is subject to the MIT license that is bundled.
+ */
+
+namespace {
+
+    class App extends \Guanguans\Coole\Facade\App
+    {
+    }
+
+    class DB extends \Guanguans\Coole\Facade\DB
+    {
+    }
+
+    class Logger extends \Guanguans\Coole\Facade\Logger
+    {
+    }
+
+    class Router extends \Guanguans\Coole\Facade\Router
+    {
+    }
+
+    class View extends \Guanguans\Coole\Facade\View
+    {
+    }
+}
