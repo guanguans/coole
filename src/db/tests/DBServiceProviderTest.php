e1cfabd752efbb4d2a30340304cffce2195ad29f
yaozm <ityaozm@gmail.com>
2021-08-31 17:52:45 +0800

    Update db tests

---
+++ src/db/tests/DBServiceProviderTest.php
@@ -0,0 +1,19 @@
+<?php
+
+/**
+ * This file is part of Coole.
+ *
+ * @link     https://github.com/guanguans/coole
+ * @contact  guanguans <ityaozm@gmail.com>
+ * @license  https://github.com/guanguans/coole/blob/main/LICENSE
+ */
+
+namespace Coole\DB\Tests;
+
+class DBServiceProviderTest extends TestCase
+{
+    public function testTrue()
+    {
+        $this->assertTrue(true);
+    }
+}
