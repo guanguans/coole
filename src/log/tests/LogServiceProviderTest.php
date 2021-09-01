a00945eb847e24ebb10ebcd17e25dda1fe9e25c4
yaozm <ityaozm@gmail.com>
2021-09-01 10:16:59 +0800

    Update log tests

---
+++ src/log/tests/LogServiceProviderTest.php
@@ -0,0 +1,19 @@
+<?php
+
+/**
+ * This file is part of Coole.
+ *
+ * @link     https://github.com/guanguans/coole
+ * @contact  guanguans <ityaozm@gmail.com>
+ * @license  https://github.com/guanguans/coole/blob/main/LICENSE
+ */
+
+namespace Coole\Log\Tests;
+
+class LogServiceProviderTest extends TestCase
+{
+    public function testTrue()
+    {
+        $this->assertTrue(true);
+    }
+}
