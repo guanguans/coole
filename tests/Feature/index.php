8c6baf626d45a24eb5a57ee88054d46e8c64004b
yaozm <ityaozm@gmail.com>
2021-09-17 18:28:18 +0800

    Add coole serve

---
+++ tests/Feature/index.php
@@ -0,0 +1,40 @@
+<?php
+
+/**
+ * This file is part of Coole.
+ *
+ * @link     https://github.com/guanguans/coole
+ * @contact  guanguans <ityaozm@gmail.com>
+ * @license  https://github.com/guanguans/coole/blob/main/LICENSE
+ */
+
+/*
+ * This file is part of the guanguans/coole.
+ *
+ * (c) guanguans <ityaozm@gmail.com>
+ *
+ * This source file is subject to the MIT license that is bundled.
+ */
+
+use Coole\Foundation\App;
+use Coole\Routing\Facade\Router;
+use Symfony\Component\HttpFoundation\Request;
+
+require __DIR__.'/../../vendor/autoload.php';
+
+// 1. Create App.
+$app = new App();
+$app['debug'] = true;
+
+// 2. Add route with closure middleware.
+Router::get('/', function () {
+    return 'This is the Coole framework.';
+})->setMiddleware(function (Request $request, Closure $next) {
+    $response = $next($request);
+    $response->headers->set('X-Coole-Version', App::version());
+
+    return $response;
+});
+
+// 3. Run service.
+$app->run();
