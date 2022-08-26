cda350b8ca08b18109461c5839b41ccb37c124e9
yaozm <ityaozm@gmail.com>
2022-08-26 18:16:58 +0800

    Add `pestphp/pest-plugin-global-assertions`

---
+++ src/config/tests/ExampleTest.php
@@ -0,0 +1,15 @@
+<?php
+
+declare(strict_types=1);
+
+/**
+ * This file is part of Coole.
+ *
+ * @link     https://github.com/guanguans/coole
+ * @contact  guanguans <ityaozm@gmail.com>
+ * @license  https://github.com/guanguans/coole/blob/main/LICENSE
+ */
+
+test('example', function () {
+    expect(true)->toBeTrue();
+});
