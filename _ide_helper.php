ced0e00129c18adaf19696715db3b86df5e98232
yaozm <ityaozm@gmail.com>
2022-09-19 00:30:25 +0800

    Update IDE helper files

--- _ide_helper.php
+++ _ide_helper.php
@@ -10,7 +10,10 @@
 
 namespace {
     class App extends  \Coole\Foundation\Facades\App{}
+    class Console extends \Coole\Console\Facades\Console{}
     class DB extends \Coole\Database\Facades\DB{}
+    class ErrorHandler extends \Coole\ErrorHandler\Facades\ErrorHandler{}
+    class EventDispatcher extends \Coole\EventDispatcher\Facades\EventDispatcher{}
     class Logger extends \Coole\Logger\Facades\Logger{}
     class Router extends \Coole\Routing\Facades\Router{}
     class View extends \Coole\View\Facades\View{}
