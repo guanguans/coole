8f98338a149007029b30ac2ee7c834809d45a319
guanguans <guanguans@users.noreply.github.com>
2022-09-18 13:32:13 +0000

    Fix styling

--- src/logger/src/Facades/Logger.php
+++ src/logger/src/Facades/Logger.php
@@ -15,18 +15,18 @@ namespace Coole\Logger\Facades;
 use Coole\Foundation\Facades\Facade;
 
 /**
- * @method static \Psr\Log\LoggerInterface channel(string $channel = null)
+ * @method static \Psr\Log\LoggerInterface    channel(string $channel = null)
  * @method static \Coole\Logger\LoggerManager shareContext(array $context)
- * @method static array sharedContext()
- * @method static void alert(string $message, array $context = [])
- * @method static void critical(string $message, array $context = [])
- * @method static void debug(string $message, array $context = [])
- * @method static void emergency(string $message, array $context = [])
- * @method static void error(string $message, array $context = [])
- * @method static void info(string $message, array $context = [])
- * @method static void log($level, string $message, array $context = [])
- * @method static void notice(string $message, array $context = [])
- * @method static void warning(string $message, array $context = [])
+ * @method static array                       sharedContext()
+ * @method static void                        alert(string $message, array $context = [])
+ * @method static void                        critical(string $message, array $context = [])
+ * @method static void                        debug(string $message, array $context = [])
+ * @method static void                        emergency(string $message, array $context = [])
+ * @method static void                        error(string $message, array $context = [])
+ * @method static void                        info(string $message, array $context = [])
+ * @method static void                        log($level, string $message, array $context = [])
+ * @method static void                        notice(string $message, array $context = [])
+ * @method static void                        warning(string $message, array $context = [])
  *
  * @mixin  \Coole\Logger\LoggerManager
  *
