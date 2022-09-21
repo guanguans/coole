17dd5b6dfd5da2eeb023484270a6bab6012712d1
yaozm <ityaozm@gmail.com>
2022-09-21 16:14:09 +0800

    Update examples

--- index.php
+++ index.php
@@ -22,6 +22,9 @@ require __DIR__.'/vendor/autoload.php';
 // 1. Create app.
 $app = new App(['debug' => true]);
 
+// Enable debugging(optional).
+// $app['config']->set('app.debug', false);
+
 // 2. Add route with closure middleware.
 Router::get('/', static fn (App $app) => "This is the Coole framework(v{$app::version()}).")
     ->setMiddleware(static function (Request $request, Closure $next) {
